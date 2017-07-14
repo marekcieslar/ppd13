542298503d2ec937a31644c323eeaaf278183697
marek <marek.cieslar@gmail.com>
2017-07-14 08:15:33 +0200

    adden infos to goals

--- ppd/dataGoals.js
+++ ppd/dataGoals.js
@@ -1,17 +1,17 @@
 const goals = [
     {
         priority: 'A',
-        name: 'praca jako programista Front-End, B2B',
+        name: 'praca jako programista Front-End, B2B<ul><li>Angular Master</li><li>Wordpress JSON Master</li><li>Ciekawe miejsca</li><li>przepisy</li><li>dzieciaki</li></ul>',
         done: false,
         until: '2017-09-01'
     }, {
         priority: 'A',
-        name: 'grupa zaufanych przyjaciół',
+        name: 'grupa zaufanych przyjaciół<ul><li>networking</li></ul>',
         done: false,
         until: '2018-06-30'
     }, {
         priority: 'A',
-        name: '40 000 zł oszczędności własnych w banku',
+        name: '40 000 zł oszczędności własnych w banku<ul><li>odkładać 10% z każdej zarobionej sumy</li><li>odkładać minimum 20% z każdej otrzymanej bez pracy sumy</br></ul>',
         done: false,
         until: '2018-06-30'
     }
